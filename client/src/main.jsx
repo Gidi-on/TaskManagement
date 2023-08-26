98ebc6669fe80318d232cccb5a76bed4f1e8aa38
Aribaba <86304459+Gidi-on@users.noreply.github.com>
2023-08-26 17:30:55 +0100

    Create main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import App from "./App.jsx";
+import { store } from "./features/store";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ToastContainer />
+    </React.StrictMode>
+  </Provider>
+);
